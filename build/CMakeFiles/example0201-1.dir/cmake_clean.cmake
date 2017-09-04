e16390824a7e9732089ff1ca8c7aa6c29f07f1b3
Double0101 <double0101@foxmail.com>
2017-09-05 01:32:57 +0800

    first

---
+++ build/CMakeFiles/example0201-1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example0201-1.dir/example0201-1.cpp.o"
+  "example0201-1.pdb"
+  "example0201-1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/example0201-1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
